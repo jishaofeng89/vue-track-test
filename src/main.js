4ab4be075529006f2eab64bb9923f8bd796d3ddf
jishaofeng89 <1195957057@qq.com>
2021-04-26 20:18:42 +0800

    add v-track

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VTrack from 'v-track';
+import trackEvents from './tracks'
 
 Vue.config.productionTip = false
+
+Vue.use(VTrack, {
+  trackEvents,
+  trackEnable: {
+    UVPV: true,
+    TONP: true
+  }
+});
 
 new Vue({
   render: h => h(App),
